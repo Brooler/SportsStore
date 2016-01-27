9ccab61b7d50416e7d24cc749236adc7af970127
Brooler <maryaninin97@hotmail.com>
2016-01-27 22:55:17 +0200

    SportsStore

---
+++ SportsStore/SportsStore.UnitTests/ImageTests.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using System.Linq;
+using System.Web.Mvc;
+using Ninject.Infrastructure.Disposal;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class ImageTests
+    {
+        [TestMethod]
+        public void Can_Retrieve_Image_Data()
+        {
+            //arrange - create a Product with image data
+            Product prod = new Product
+            {
+                ProductID = 2,
+                Name = "Test",
+                ImageData = new byte[] {},
+                ImageMimeType = "image/png"
+            };
+            //arrange - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                prod,
+                new Product {ProductID = 3, Name = "P3"}
+            }.AsQueryable());
+            //arrange - create the controller
+            ProductController target = new ProductController(mock.Object);
+            //act - call the GetImage action method
+            ActionResult result = target.GetImage(2);
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof (FileResult));
+            Assert.AreEqual(prod.ImageMimeType, ((FileResult)result).ContentType);
+        }
+
+        [TestMethod]
+        public void Cannot_Retrieve_Image_Data_For_Invalid_ID()
+        {
+            //arrange - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"}
+            }.AsQueryable());
+            //arrange - create the controller
+            ProductController target = new ProductController(mock.Object);
+            //act - call the GetImage action method
+            ActionResult result = target.GetImage(100);
+            //assert
+            Assert.IsNull(result);
+        }
+    }
+}
---
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Moq;
+using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Concrete;
+using System.Configuration;
+using SportsStore.WebUI.Infrastructure.Abstract;
+using SportsStore.WebUI.Infrastructure.Concrete;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+
+        public NinjectControllerFactory()
+        {
+            //container creating
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext,
+            Type controllerType)
+        {
+            //getting controller object from container using it's type
+            return controllerType == null ? null : (IController) ninjectKernel.Get(controllerType);
+        }
+
+        private void AddBindings()
+        {
+            //container configuration
+            ninjectKernel.Bind<IProductRepository>().To<EFProductRepository>();
+            EmailSettings emailSettings = new EmailSettings
+            {
+                WriteAsFile = bool.Parse(ConfigurationManager
+                .AppSettings["Email.WriteAsFile"] ?? "false")
+            };
+            ninjectKernel.Bind<IOrderProcessor>()
+                .To<EmailOrderProcessor>()
+                .WithConstructorArgument("settings", emailSettings);
+            ninjectKernel.Bind<IAuthProvider>().To<FormsAuthProvider>();
+        }
+    }
+}
